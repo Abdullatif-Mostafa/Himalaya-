f2a57bfef1784fe56564cd7cfa2c4cd48f229aa3
Tifa <abdellatyfmostafa8@gmail.com>
2023-12-11 01:42:28 +0200

    there is new updates

---
+++ js.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {
+    scrollFunction();
+  };
+console.log("data")
+  function scrollFunction() {
+    var navbar = document.getElementById("navbar");
+  console.log(navbar)
+    if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  }
